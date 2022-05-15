c5052282928eca88b9c09694b35df925247ea3cb
Yohan Lee <io.yohanlee@gmail.com>
2022-05-15 09:41:52 -0400

    Exercise 1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -10,9 +10,32 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
-      <SuperHeader>
-        <Row>
+    <Wrapper>
+      <MobileHeader>
+        <SuperHeader>
+          <Row>
+            <ActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+            </ActionGroup>
+            <ActionGroup>
+              <button>
+                <User size={24} />
+              </button>
+            </ActionGroup>
+          </Row>
+        </SuperHeader>
+        <MainHeader>
+          <Logo />
+        </MainHeader>
+      </MobileHeader>
+
+      <DesktopHeader>
+        <MainHeader>
           <ActionGroup>
             <button>
               <Search size={24} />
@@ -21,19 +44,33 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
+          <Logo />
+          <CTAButtonGroup>
+            <Button>Subscribe</Button>
+            <Link href="/sign-up">Already a subscriber?</Link>
+          </CTAButtonGroup>
+        </MainHeader>
+      </DesktopHeader>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`
+`;
+
+const MobileHeader = styled.div`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`
+
+const DesktopHeader = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
@@ -59,12 +96,30 @@ const ActionGroup = styled.div`
   }
 `;
 
+const CTAButtonGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  margin-top: 20px;
+`
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: space-between;
+  }
 `;
+
+const Link = styled.a`
+  text-decoration: underline;
+  color: ${COLORS.gray[900]};
+  font-size: ${14 / 16}rem;
+`
 
 export default Header;
